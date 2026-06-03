897746fcf7503e1af38ddb23b1934ea0d168328f
Aayan Vikram Singh <147069249+aayanvikramsingh@users.noreply.github.com>
2026-06-03 22:02:23 +0530

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l=[[num,i]for i,num in enumerate(nums)]
+        l.sort()
+
+        left,right=0,len(nums)-1
+        while(left<right):
+            cur=l[left][0]+l[right][0]
+            if(cur==target):
+                return [min(l[left][1],l[right][1]),max(l[left][1],l[right][1])]
+            elif cur<target:
+                left+=1
+            else:
+                right-=1
+        return []
+        
